4965328f58165944f048c4a6f9b2fcff8fdab687
mpdeimos <martin.deimos@gmx.de>
2011-07-25 17:42:42 +0200

    adding some more stuff

---
+++ res/js/ie.js
@@ -0,0 +1,7 @@
+(function(){
+ var e = ("abbr,article,aside,audio,canvas,datalist,details," +
+    "figure,footer,header,hgroup,mark,menu,meter,nav,output," +
+    "progress,section,time,video").split(',');
+  for (var i = 0; i < e.length; i++) {
+    document.createElement(e[i]);
+  })();
